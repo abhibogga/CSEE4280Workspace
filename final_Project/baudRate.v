184428b4b5461f34cfb411f499895ea959d50c5e
abhi boggavarapu <abhibogga@gmail.com>
2025-02-25 23:29:38 -0500

    making final proj folder, not much in here yet

---
+++ final_Project/baudRate.v
@@ -0,0 +1,15 @@
+
+
+module baudRate(clk, outBaud, rst);
+
+    //Define inputs here: 
+    input clk; 
+    input rst; 
+
+    //Defind outputs here: 
+    output reg outBaud; 
+
+    //We know that our internal clock is 100MHz and baudRate is 9600 so we won't try and make this module universal so we can do all math in hosue
+    //100M/9600 = 
+
+endmodule
